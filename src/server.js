a6e5b43684864f4ff162f9d8eeae8f8478380715
Tenebris An <y2222672@gmail.com>
2026-03-26 19:47:34 +0200

    refactor: move pino configuration to separate logger middleware

--- src/server.js
+++ src/server.js
@@ -1,22 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
 
 const PORT = Number(process.env.PORT) || 4000;
 
 const app = express();
 
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+app.use(logger);
 app.use(
   cors({
     origin: process.env.FRONTEND_DOMAIN || 'http://localhost:3000',
