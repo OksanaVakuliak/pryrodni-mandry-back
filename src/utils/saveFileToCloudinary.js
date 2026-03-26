00de99c1891fea6dc297bb931939b1efe416323f
Vonels <klizka38@gmail.com>
2026-03-26 22:50:40 +0200

    refactor: enhance cloudinary upload with dynamic folder and settings

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -4,11 +4,20 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
-export const saveFileToCloudinary = async (filePath) => {
+export const saveFileToCloudinary = async (
+  filePath,
+  folder = 'general',
+  publicId = 'file',
+) => {
   const result = await cloudinary.uploader.upload(filePath, {
-    folder: 'pryrodni-mandry',
+    folder: `pryrodni-mandry/${folder}`,
+    resource_type: 'image',
+    public_id: publicId,
+    overwrite: true,
+    unique_filename: false,
   });
 
   return result.secure_url;
