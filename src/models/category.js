0f07816d0459dc329c4b0da358728ac4831cfe3a
Vonels <klizka38@gmail.com>
2026-03-26 20:33:24 +0200

    my

--- src/models/category.js
+++ src/models/category.js
@@ -1,10 +1,1 @@
-import mongoose from 'mongoose';
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-});
-
-export default mongoose.model('Category', categorySchema);
