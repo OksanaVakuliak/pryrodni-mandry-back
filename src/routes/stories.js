3ac9e1a48bfb6ef6c5448da49f2327a1cf7194ea
Dmytro <dimalifecell2019@gmail.com>
2026-03-27 18:09:08 +0200

    fix: remove merge conflict markers from stories.js

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
 import getAllStories from '../controllers/storyController.js';
@@ -9,5 +8,3 @@ const storiesRouter = Router();
 storiesRouter.get('/', celebrate(getAllStoriesSchema), getAllStories);
 
 export default storiesRouter;
-=======
->>>>>>> b0ad85161e7762ac11bef9f137b8c098d513105b
