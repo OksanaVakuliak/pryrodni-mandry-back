4a673f7aa9f5f77d76428c03c8ba046bc8fd8448
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-03-27 14:02:22 +0100

    Revert "Merge pull request #25 from OksanaVakuliak/feat/Implement-Paginated-and-Filtered-Stories-List#10"
    This reverts commit 33af60176bb5fc7c93ab9650f83382647ddf8c80, reversing
    changes made to f226beb61cd5efdc50de108371cb4557168d3dff.

--- src/routes/stories.js
+++
@@ -1,8 +0,0 @@
-import { celebrate } from 'celebrate';
-import { Router } from 'express';
-import getAllStories from '../controllers/storyController.js';
-import { getAllStoriesSchema } from '../validations/storyValidation.js';
-
-const storiesRouter = Router();
-
-storiesRouter.get('/stories', celebrate(getAllStoriesSchema), getAllStories);
