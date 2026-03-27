1cad979ac07372037cc74b94da8f73a09bd07e18
Dmytro <dimalifecell2019@gmail.com>
2026-03-27 21:37:19 +0200

    feat: add getPopularStories and getRecommendedStories functions to storyController

--- src/controllers/storyController.js
+++ src/controllers/storyController.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import Story from '../models/story.js';
 import createHttpError from 'http-errors';
 
-const getAllStories = async (req, res) => {
+export const getAllStories = async (req, res) => {
   const { page = 1, perPage = 6, category } = req.query;
   const skip = (page - 1) * perPage;
 
@@ -39,4 +39,37 @@ const getAllStories = async (req, res) => {
   });
 };
 
-export default getAllStories;
+export const getPopularStories = async (req, res) => {
+  const popularStories = await Story.find({
+    'savedByUsers.0': { $exists: true },
+  })
+    .sort({ rate: -1 })
+    .limit(10)
+    .populate('ownerId', 'name avatar')
+    .populate('category', 'name');
+
+  res.status(200).json(popularStories);
+};
+
+export const getRecommendedStories = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw createHttpError(400, 'Invalid story ID');
+  }
+
+  const currentStory = await Story.findById(id);
+  if (!currentStory) {
+    throw createHttpError(404, 'Story not found');
+  }
+
+  const stories = await Story.find({
+    category: currentStory.category,
+    _id: { $ne: id },
+  })
+    .limit(3)
+    .populate('ownerId', 'name avatar')
+    .populate('category', 'name');
+
+  res.status(200).json(stories);
+};
