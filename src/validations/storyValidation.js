f6838f91ea69530d39c95c7f2b3678bb7a399b79
Dmytro <dimalifecell2019@gmail.com>
2026-03-28 00:32:46 +0200

    feat: add objectId validation for patch save story schema

--- src/validations/storyValidation.js
+++ src/validations/storyValidation.js
@@ -1,9 +1,20 @@
 import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
 
 export const getAllStoriesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(1).max(100).default(6),
     category: Joi.string().hex().length(24),
+  }),
+};
+
+export const patchSaveStorySchema = {
+  [Segments.PARAMS]: Joi.object({
+    id: Joi.string().custom(objectIdValidator).required(),
   }),
 };
