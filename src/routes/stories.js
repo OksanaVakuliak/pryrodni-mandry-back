fc4e5b4230f86ec0ea54ce9df6424ce7dc7cc293
Dmytro <dimalifecell2019@gmail.com>
2026-03-27 21:37:47 +0200

    feat: update stories route to include popular and recommended stories endpoints

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -1,10 +1,22 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
-import getAllStories from '../controllers/storyController.js';
+import {
+  getAllStories,
+  getPopularStories,
+  getRecommendedStories,
+} from '../controllers/storyController.js';
 import { getAllStoriesSchema } from '../validations/storyValidation.js';
 
 const storiesRouter = Router();
 
 storiesRouter.get('/stories', celebrate(getAllStoriesSchema), getAllStories);
+
+storiesRouter.get(
+  '/stories/popular',
+  celebrate(getAllStoriesSchema),
+  getPopularStories,
+);
+
+storiesRouter.get('/stories/:id/recommended', getRecommendedStories);
 
 export default storiesRouter;
