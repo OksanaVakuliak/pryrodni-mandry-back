ce9f9523a25d70b5e61a7de78b0fd5ed977d7854
Dmytro <dimalifecell2019@gmail.com>
2026-03-27 18:08:41 +0200

    fix: resolve merge conflict in storyController.js

--- src/controllers/storyController.js
+++ src/controllers/storyController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose';
 import Story from '../models/story.js';
 import createHttpError from 'http-errors';
@@ -41,5 +40,3 @@ const getAllStories = async (req, res) => {
 };
 
 export default getAllStories;
-=======
->>>>>>> b0ad85161e7762ac11bef9f137b8c098d513105b
