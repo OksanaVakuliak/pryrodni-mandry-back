bf3865b66cee6c8d54723ced6c38a827a3bc5d2c
Dmytro <dimalifecell2019@gmail.com>
2026-03-27 23:45:25 +0200

    feat: simplify popular stories route and correct delete story endpoint

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -14,16 +14,12 @@ const storiesRouter = Router();
 
 storiesRouter.get('/stories', celebrate(getAllStoriesSchema), getAllStories);
 
-storiesRouter.get(
-  '/stories/popular',
-  celebrate(getAllStoriesSchema),
-  getPopularStories,
-);
+storiesRouter.get('/stories/popular', getPopularStories);
 
 storiesRouter.get('/stories/:id/recommended', getRecommendedStories);
 
 storiesRouter.post('/stories/:id/save', authenticate, postSaveStory);
 
-storiesRouter.delete('/stories/:id/save', authenticate, deleteSaveStory);
+storiesRouter.delete('/stories/:id/delete', authenticate, deleteSaveStory);
 
 export default storiesRouter;
