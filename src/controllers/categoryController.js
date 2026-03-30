55475c2552c9a601a760bb2a0d5dd27fcb553258
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-03-30 21:30:52 +0200

    refactor: remove error handling for empty categories in getAllCategories function

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,12 +1,7 @@
 import Category from '../models/category.js';
-import createHttpError from 'http-errors';
 
 export const getAllCategories = async (req, res) => {
   const categories = await Category.find({}).sort({ category: 1 });
-
-  if (!categories) {
-    throw createHttpError(404, 'Categories not found');
-  }
 
   return res.status(200).json(categories);
 };
