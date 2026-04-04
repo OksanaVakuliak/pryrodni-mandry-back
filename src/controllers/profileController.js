12daa0644ec7a597c8658d4a03799e2b510daa04
Tenebris An <y2222672@gmail.com>
2026-04-04 23:05:04 +0300

    feat: implement protected avatar update endpoint with Cloudinary

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,7 +1,10 @@
 import Story from '../models/story.js';
+import User from '../models/user.js';
 import { parsePagination, getPaginationMeta } from '../utils/pagination.js';
+import { v2 as cloudinary } from 'cloudinary';
+import createHttpError from 'http-errors';
 // import bcrypt from 'bcrypt';
-// import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const getMyProfile = async (req, res) => {
   const user = req.user;
@@ -82,7 +85,41 @@ export const getSavedStories = async (req, res) => {
 
   res.status(200).json(response);
 };
+export const updateAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      throw createHttpError(400, 'Файл не завантажено');
+    }
 
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw createHttpError(404, 'Користувача не знайдено');
+    }
+
+    if (user.avatarUrl && user.avatarUrl.includes('cloudinary')) {
+      const publicId = user.avatarUrl
+        .split('/')
+        .slice(-2)
+        .join('/')
+        .split('.')[0];
+      await cloudinary.uploader.destroy(publicId);
+    }
+    const avatarUrl = await saveFileToCloudinary(req.file, 'avatars');
+
+    user.avatarUrl = avatarUrl;
+    await user.save();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Аватар успішно оновлено',
+      data: { avatarUrl: user.avatarUrl },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 // export const updateProfile = async (req, res, next) => {
 //   const updates = {};
 
