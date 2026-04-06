e9b4371e827883fecc2916f9760b6d7df2cfab9a
Valeri <valeriakoval27072007@gmail.com>
2026-04-06 23:34:34 +0200

    fix:fixed validation

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -2,12 +2,8 @@ import { Joi, Segments } from 'celebrate';
 
 export const confirmProfileUpdateSchema = {
   [Segments.BODY]: Joi.object({
-    token: Joi.string()
-      .base64({ paddingRequired: false })
-      .regex(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/)
-      .required()
-      .messages({
-        'string.pattern.base': 'Невірний формат токена',
-      }),
+    token: Joi.string().required().messages({
+      'string.pattern.base': 'Невірний формат токена',
+    }),
   }),
 };
